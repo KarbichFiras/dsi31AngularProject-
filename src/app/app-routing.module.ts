b5c59c4b0342f6054b6ee906709635f4249f8c80
KarbichFiras <firaskarbich@gmail.com>
2021-11-07 17:25:30 +0100

    panier routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule , Routes } from '@angular/router';
 import {PaymentsComponent } from "./payments/payments.component";
 import {AddfoodComponent} from "./addfood/addfood.component";
+import {PanierComponent} from "./panier/panier.component";
+
   const routes : Routes =[  
     {path :"payments" , component : PaymentsComponent },
     {path :"addfood" , component : AddfoodComponent },
+    {path :"panier" , component : PanierComponent },
   ];
 
 @NgModule({
