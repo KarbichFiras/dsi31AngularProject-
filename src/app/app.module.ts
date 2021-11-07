76af417dfaa6a0dea4adf65d287bc334992a8e70
ihebessaied <i.essaied@yahoo.fr>
2021-11-07 17:15:00 +0100

    add add food routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PaymentsComponent } from './payments/payments.component';
+import { AddfoodComponent } from './addfood/addfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PaymentsComponent
+    PaymentsComponent,
+    AddfoodComponent
   ],
   imports: [
     BrowserModule,
